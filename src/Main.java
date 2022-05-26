bb462321615f936583029a02b3708b58f13241d7
FernandoPerera <fernando02perera@gmail.com>
2022-05-26 10:39:03 +0100

    Feature: Hechos los métodos en la clase VeredaGT implementados de la interfaz, la clase VeredaGT instanciada en el Main y comprobados los métodos

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        VeredaGT cochito = new VeredaGT("VA 7777 P", 400);
+
+        System.out.println("\n\t---------------------------------------");
+        System.out.println("\tLa energía del vehículo es  " + cochito.energyLevel());
+        System.out.println("\t---------------------------------------");
+
+        cochito.move();
+        cochito.move();
+
+        System.out.println(cochito.status());
+
+        System.out.println("\n\t---------------------------------------");
+        System.out.println("\tLa energía del vehículo es  " + cochito.energyLevel());
+        System.out.println("\t---------------------------------------");
+
+        cochito.move();
+
+        cochito.recharge();
+
+        System.out.println("\n\t---------------------------------------");
+        System.out.println("\tLa energía del vehículo es  " + cochito.energyLevel());
+        System.out.println("\t---------------------------------------");
+
+        System.out.println(cochito.status());
+
+
     }
 }
--- src/VeredaGT.java
+++ src/VeredaGT.java
@@ -3,30 +3,67 @@ public class VeredaGT implements Vehicle{
     private String matricula;
     private static final float MAX_ENERGY = 600;
     private float energy;
-    private int movementCount;
+    private int movementCount = 0;
     private static final float ENERGY_DELTA = 50;
 
-    public VeredaGT(String matricula) {
+    public VeredaGT(String matricula, float energy) {
         this.matricula = matricula;
+        this.energy = energy;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public float getEnergy() {
+        return energy;
+    }
+
+    public int getMovementCount() {
+        return movementCount;
     }
 
     @Override
     public float energyLevel() {
-        return 0;
+        return getEnergy();
     }
 
     @Override
     public void recharge() {
+
+        if (energy == MAX_ENERGY){
+            System.out.println("La energía ya está al máximo");
+        } else {
+            energy = MAX_ENERGY;
+
+            System.out.println("\n\t=======================================");
+            System.out.println("\tSe ha recargado la energía del vehículo");
+            System.out.println("\t=======================================");
+        }
 
     }
 
     @Override
     public void move() {
 
+        if (energy < ENERGY_DELTA ) {
+            System.out.println("\n\t----------------------------------------");
+            System.out.println("El vehículo no puede realizar movimientos " +
+                                "no tiene la energía necesaria");
+        } else {
+            energy -= ENERGY_DELTA;
+            System.out.println("\n | Se va ha mover el vehículo... |");
+
+            movementCount++;
+        }
     }
 
     @Override
     public String status() {
-        return null;
+
+
+
+        return "\nEl vehículo " + getMatricula() + " tiene un nivel de carga de " +
+                getEnergy() + " y se ha movido " + getMovementCount() + " veces";
     }
 }
